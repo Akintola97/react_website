dbd87a53b49073da460a01afede7f4bd3eada77a
Akintola97 <akinoyetayo97@gmail.com>
2022-06-09 03:23:59 -0500

    update

--- src/components/Video.js
+++ src/components/Video.js
@@ -5,22 +5,23 @@ import './Video.css'
 import earthVideo from '../assets/earthVideo.mp4'
 
 
-function video() {
+const video = () => {
   return (
     <div className='earthVid'>
-        <video autoPlay loop muted id='vid'>
-            <source src ={earthVideo} type='video/mp4'></source>
-        </video>
-        <div className='vid-overlay'>
-            <h1>Travel</h1>
-            <p>Akin's space Travels</p>
+    <video autoPlay loop muted id='video'>
+        <source src={earthVideo} type='video/mp4' />
+    </video>
+    <div className='vid-overlay'>
+        <h1>Travel</h1>
+        <p>Akin's space travel</p>
         <div>
             <Link to='/training' className='btn'>Training</Link>
             <Link to='/contact' className='btn btn-light'>Launch</Link>
         </div>
-        </div>
-        </div>
-  )
-}
+    </div>
+    </div>
+    )
+    }
+
 
 export default video
